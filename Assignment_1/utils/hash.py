7f2ea96615d5964792bcb7cc8718db0aad6e38e7
Adilzhan Slyamgazy <herztard@gmail.com>
2025-02-23 15:56:18 +0500

    A1, T2

--- Assignment_1/utils/hash.py
+++ Assignment_1/utils/hash.py
@@ -4,12 +4,18 @@ from Crypto.Hash import MD4
 def get_hash(byte_string: bytes, block_size: int):
     # TODO: Generate hash using MD4 and truncate the hash to the given block_size
     ### YOUR CODE ###
+    md4 = MD4.new()
+    md4.update(byte_string)
+    return md4.digest()[:block_size]
 
 
 def get_hash_salted(byte_string: bytes, salt: bytes, block_size: int):
     # TODO: Generate salted hash using MD4
     # Hint: truncate the hash to the given size: block_size - len(salt)
     ### YOUR CODE ###
+    md4 = MD4.new()
+    md4.update(byte_string + salt)
+    return md4.digest()[:block_size - len(salt)]
 
 
 def verify_hash(byte_string: bytes, hash: bytes):
